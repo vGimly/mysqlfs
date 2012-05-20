779852a34b2da13dd3c55f494bf6016d12f124ee
Andrea Brancatelli <abrancatelli@schema31.it>
2012-05-20 16:32:01 +0200

    Creating SW_DETAILS table. Also to check the DB Update :)

---
+++ sql/updates/00000001.sql
@@ -0,0 +1,16 @@
+-- Bogus BEGIN since TABLE definitions are not transaction-safe.
+BEGIN;
+
+-- Creating Table
+CREATE TABLE `SW_DETAILS` (
+  `KEY` VARCHAR(200) NOT NULL,
+  `VALUE` VARCHAR(200) NOT NULL,
+  `LAST_CHANGE` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
+  PRIMARY KEY (`KEY`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- First record
+REPLACE INTO SW_DETAILS SET KEY = "VERSION", VALUE = "0.4.2beta";
+
+-- Commit everything
+COMMIT;
