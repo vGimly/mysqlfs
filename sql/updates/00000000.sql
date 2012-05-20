3231c02b8ce8d9896620e833890b35992e0dcf83
Andrea Brancatelli <abrancatelli@schema31.it>
2012-05-20 16:13:29 +0200

    Added some comments just to pretend i care

--- sql/updates/00000000.sql
+++ sql/updates/00000000.sql
@@ -1,8 +1,14 @@
+-- Bogus BEGIN since TABLE definitions are not transaction-safe.
+BEGIN;
 
+-- Creating Table
 CREATE TABLE `DATABASE_VERSION` (
   `CURRENT_VERSION` int(8) NOT NULL,
   `LAST_CHANGE` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- First record
 INSERT INTO DATABASE_VERSION SET CURRENT_VERSION = 0, LAST_CHANGE = NOW();
 
+-- Commit everything
+COMMIT;
