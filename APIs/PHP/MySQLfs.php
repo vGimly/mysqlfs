0c0d305c0ee9b590541ca4d44ff335d908551125
Andrea Brancatelli <abrancatelli@schema31.it>
2012-06-11 00:44:48 +0200

    API and Example to save a file into the DB. Tested and working ;)

--- APIs/PHP/MySQLfs.php
+++ APIs/PHP/MySQLfs.php
@@ -151,6 +151,69 @@ class MySQLfs {
     }
 
     /**
+     * Create a new file at the specified parent
+     *
+     * @author Andrea Brancatelli <andrea@brancatelli.it>
+     * @package MySQLfsAPI
+     * @param string $name filename
+     * @param string $parent the folder containing it
+     * @return integer the inode of the created file
+     */
+    function createFile($name, $parent)
+    {
+	$query  = "INSERT INTO tree SET name = '".$name."', parent = '".$parent."'";
+	$affected =& $this->dbLink->exec($query);
+	if (PEAR::isError($affected)) { die($affected->getMessage()); }	
+
+	$inodeArray =  $this->locateInode($name, $parent);
+	$inode = $inodeArray["inode"];
+
+	$query  = "INSERT INTO inodes SET inode = '".$inode."', 
+					  inuse = 0,
+					  deleted = 0,
+					  mode = 33204,
+					  uid = 0,
+					  gid = 0,
+					  atime = UNIX_TIMESTAMP(NOW()),
+					  ctime = UNIX_TIMESTAMP(NOW()),
+					  mtime = UNIX_TIMESTAMP(NOW()),
+					  size = 0";
+	$affected =& $this->dbLink->exec($query);
+	if (PEAR::isError($affected)) { die($affected->getMessage()); }	
+
+	return $inode;
+    }
+
+    /**
+     * Add a block of data to an inode
+     * NOTE: this function DOES NOT (yet?) take in consideration
+     * partially-filled blocks!
+     * It should be pretty uncommon in a PHP API to append to an
+     * already existing file but, hey, an API is an API, so this
+     * needs to be fixed.
+     *
+     * @author Andrea Brancatelli <andrea@brancatelli.it>
+     * @package MySQLfsAPI
+     * @param integer $inode the inode to add data
+     * @param string $content the data to add
+     * @return TRUE or FALSE
+     */
+    function addBlock($inode, $content)
+    {
+	$seq = $this->getSingleValue("SELECT COUNT(*) FROM data_blocks WHERE inode = '".$inode."'");
+
+	$query  = "INSERT INTO data_blocks SET data = '".addslashes($content)."', inode = '".$inode."', seq = '".$seq."'";
+	$affected =& $this->dbLink->exec($query);
+	if (PEAR::isError($affected)) { die($affected->getMessage()); }	
+
+	$size = $this->getSingleValue("SELECT SUM(OCTET_LENGTH(data)) FROM data_blocks WHERE inode = '".$inode."'");
+
+	$query  = "UPDATE inodes SET size = '".$size."' WHERE inode = '".$inode."'";
+	$affected =& $this->dbLink->exec($query);
+	if (PEAR::isError($affected)) { die($affected->getMessage()); }	
+    }
+
+    /**
      * Returns a multidimensional array with all the files/dirs in a specific inode
      *
      * @author Andrea Brancatelli <andrea@brancatelli.it>
@@ -249,6 +312,41 @@ class MySQLfs {
 	}
 
 	return $ret;
+
+     }
+
+     /**
+      * Writes the received string in a specific path
+      * Has to check if the specified path already exists.
+      * 
+      * @author Andrea Brancatelli <andrea@brancatelli.it>
+      * @package MySQLfsAPI
+      * @param string $content the file content
+      * @param string $path the destination path
+      * @return bool TRUE or FALSE :)
+      */
+     function saveFile($content, $path)
+     {
+
+        $tree = $this->explodePath($path);
+
+        $inode = NULL;
+        foreach ($tree as $branch)
+        {
+		$parent = $inode;
+                $thisBranch = $this->locateInode($branch, $inode);
+                $inode = $thisBranch["inode"];
+        }
+
+	if ($inode == "")
+	{
+		$inode = $this->createFile($branch, $parent);
+		$block = str_split($content, 131072);
+		foreach ($block as $EachBlock)
+			$this->addBlock($inode, $EachBlock);
+	}
+	else
+		return FALSE;
 
      }
 
